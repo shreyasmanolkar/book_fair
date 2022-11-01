2898fa9176e72c40ce06f009d1388128a3732efc
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-01 11:32:17 +0530

    sign up updated

--- src/routes/buyers/buyers.controller.js
+++ src/routes/buyers/buyers.controller.js
@@ -20,6 +20,27 @@ async function buyerSignup(req, res){
             )`,
             [full_name, email]
         );
+
+        let buyer = await pool.query(
+            `SELECT buyer_id 
+            FROM "public"."buyers" 
+            WHERE full_name = $1`,
+            [full_name]
+        );
+
+        const buyerId = buyer.rows.map(buy => buy.buyer_id);
+
+        console.log(buyerId[0]);
+
+        pool.query(
+            `INSERT INTO carts (
+                buyer_id
+            ) VALUES (
+                $1
+            )`,
+            [buyerId[0]]
+        );
+
         res.json("Buyer Signed up");
     } catch(err){
         console.log(err);
@@ -57,9 +78,19 @@ async function buyerLogin(req, res){
     }
 };
 
-function buyerProfile(req, res){
+async function buyerProfile(req, res){
     const buyerId = Number(req.params.buyerId);
-    res.send(`Buyer id is ${buyerId}`);
+
+    const buyerDetails = await pool.query(
+        `SELECT b.buyer_id, b.full_name, b.email, c.cart_id  
+        FROM "public"."buyers" AS "b"
+        JOIN "public"."carts" AS "c" 
+        ON b.buyer_id = c.buyer_id
+        WHERE b.buyer_id = $1;`,
+        [buyerId]
+    );
+
+    res.json(buyerDetails.rows);
 };
 
 function buyerCart(req, res){
