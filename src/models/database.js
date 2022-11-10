2ee5e2cfe05e9685013322acd73e19dec149870b
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-11 00:32:34 +0530

    railway config

--- src/models/database.js
+++ src/models/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'bookfair',
-    password: 'root',
-    port: 5432
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
 });
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('./app');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+
 
 const server = http.createServer(app);
 
